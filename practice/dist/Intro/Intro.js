7b9638b0e73ecf9b23d0f5d4ff2532f2afb25913
Vipin <vipinbhardwal286@gmail.com>
2026-02-05 23:17:51 +0530

    ignore dist folder

--- practice/dist/Intro/Intro.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-console.log("hello! welcome to Typescript");
-// let a = 30
-// let a = 40 
-/*
-type script not allowed redeclaration of a variable
-**/
-// and basically it check the types and provies type security.
