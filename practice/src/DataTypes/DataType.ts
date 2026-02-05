b3367bdfa4393708d586b32682a0679afade8196
Vipin <vipinbhardwal286@gmail.com>
2026-02-05 23:39:25 +0530

    theory of data types

---
+++ practice/src/DataTypes/DataType.ts
@@ -0,0 +1,32 @@
+/*
+Data types 
+
+    1- Primitive datatypes- number, string, boolean
+    2- reference datatype-
+        Array
+        Tuples
+        Enums
+        Any , Unknown, Null , Undefined, Never
+
+--------------------------------------------------------- 
+
+we can copy premitive datatype: directly
+we can not copy reference datatype: directly
+
+
+
+#### now let's start basic types:
+
+    1- number
+        let a = 30;
+
+    2- string
+         let b = "hello" 
+     
+    3- boolean
+        true & false      
+
+
+
+
+**/
